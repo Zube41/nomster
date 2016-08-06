1267e981d96d6f65534c61df2ba34c127b791e37
vagrant <vagrant@precise32.(none)>
2016-08-06 01:47:28 +0000

    comment page

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,11 @@
 class Place < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  
-  geocoded_by :address, presence: true, length: { minimum: 3}
+
+  geocoded_by :address
   after_validation :geocode
 
-  validates :name, presence: true
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
 end
