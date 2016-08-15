8d9804c13d45abcffd81ecc6ad49e336aa64ca12
vagrant <vagrant@precise32.(none)>
2016-08-15 22:53:58 +0000

    fixed carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811011716) do
+ActiveRecord::Schema.define(version: 20160815224842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20160811011716) do
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
+    t.string   "Picture"
   end
 
   create_table "places", force: true do |t|
@@ -62,6 +64,8 @@ ActiveRecord::Schema.define(version: 20160811011716) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "photo"
+    t.string   "photos"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
