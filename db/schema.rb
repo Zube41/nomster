16a76738d957b103ce49fbc7d1e5ad52149790ac
vagrant <vagrant@precise32.(none)>
2016-08-16 00:08:20 +0000

    adjust migrations like instructed in the forum

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20160815224842) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
-    t.string   "Picture"
   end
 
   create_table "places", force: true do |t|
@@ -63,9 +62,6 @@ ActiveRecord::Schema.define(version: 20160815224842) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "photo"
-    t.string   "photos"
-    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
